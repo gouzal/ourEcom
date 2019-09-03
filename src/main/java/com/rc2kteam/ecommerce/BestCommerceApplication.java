b7d969c95ba7c78dbedc0cbd5af97afcec93746c
gouzal <larbi-inco@live.fr>
2019-09-03 14:47:56 +0100

    initil commit

---
+++ src/main/java/com/rc2kteam/ecommerce/BestCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.rc2kteam.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestCommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rc2kteam/ecommerce/BestCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rc2kteam.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
